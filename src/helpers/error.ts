1b7ddfa38417bb04f3f390adb2ba673fb1b3a6e8
kiwili <kiwilili@163.com>
2019-11-26 11:17:59 +0800

    feat(helpers/error.ts types/index.ts): create AxiosError class

---
+++ src/helpers/error.ts
@@ -0,0 +1,40 @@
+import { AxiosRequestConfig, AxiosResponse } from '../types'
+
+export class AxiosError extends Error {
+  isAxiosError: boolean
+  config: AxiosRequestConfig
+  code?: string | null
+  request?: any
+  response?: AxiosResponse
+
+  constructor(
+    message: string,
+    config: AxiosRequestConfig,
+    code?: string | null,
+    request?: any,
+    response?: AxiosResponse
+  ) {
+    super(message)
+
+    this.config = config
+    this.code = code
+    this.request = request
+    this.response = response
+    this.isAxiosError = true
+
+    // https://github.com/microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    Object.setPrototypeOf(this, AxiosError.prototype)
+  }
+}
+
+// 为了使用方便
+export function createError(
+  message: string,
+  config: AxiosRequestConfig,
+  code?: string | null,
+  request?: any,
+  response?: AxiosResponse
+) {
+  const error = new AxiosError(message, config, code, request, response)
+  return error
+}
